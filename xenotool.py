e5d409283867674096dae6fdd5f7cb73bae50727
Xenohshs <bironjohnsteve67@gmail.com>
2025-03-05 04:15:35 +0800

    Update xenotool.py

--- xenotool.py
+++ xenotool.py
@@ -1,6 +1,6 @@
 import requests
 
-__ENDPOINT_URL__: str = "https://xeno1.squareweb.app/api"
+__ENDPOINT_URL__: str = "https://cpmtermux.squareweb.app/api"
 
 class CpmTermux:
     def __init__(self, access_key) -> None:
