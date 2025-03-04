c36e6e6ab67aee75c82e88172ab469085abe5ac9
Xenohshs <bironjohnsteve67@gmail.com>
2025-03-05 04:02:01 +0800

    Update and rename Xenotool.py to XenomainTool.py

--- XenomainTool.py
+++ XenomainTool.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from xenocpm import Tresehshs
+from xenotool import CpmTermux
 
 __CHANNEL_USERNAME__ = "XenoMainChannel"
 __GROUP_USERNAME__   = "XenoMainChat"
@@ -139,7 +139,7 @@ if __name__ == "__main__":
         acc_password = prompt_valid_value("[bold][?] Account Password[/bold]", "Password", password=False)
         acc_access_key = prompt_valid_value("[bold][?] Access Key[/bold]", "Access Key", password=False)
         console.print("[bold cyan][%] Trying to Login[/bold cyan]: ", end=None)
-        cpm = Tresehshs(acc_access_key)
+        cpm = CpmTermux(acc_access_key)
         login_response = cpm.login(acc_email, acc_password)
         if login_response != 0:
             if login_response == 100:
